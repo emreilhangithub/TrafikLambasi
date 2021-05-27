ba45e7ad69aa63f746009f7a9c9ed60ffa3ab856
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-27 15:35:33 +0300

    Bu projemde sesli ve görüntülü trafik labması yaptım

--- TrafikLambası/Form1.Designer.cs
+++ TrafikLambası/Form1.Designer.cs
@@ -30,12 +30,139 @@ namespace TrafikLambası
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RedPic = new System.Windows.Forms.PictureBox();
+            this.GreenPic = new System.Windows.Forms.PictureBox();
+            this.YellowPic = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RedPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YellowPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.RedPic);
+            this.groupBox1.Controls.Add(this.GreenPic);
+            this.groupBox1.Controls.Add(this.YellowPic);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Font = new System.Drawing.Font("Bernard MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 75);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(922, 515);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Trafik Lambası";
+            // 
+            // RedPic
+            // 
+            this.RedPic.Image = ((System.Drawing.Image)(resources.GetObject("RedPic.Image")));
+            this.RedPic.Location = new System.Drawing.Point(381, 36);
+            this.RedPic.Name = "RedPic";
+            this.RedPic.Size = new System.Drawing.Size(166, 328);
+            this.RedPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.RedPic.TabIndex = 6;
+            this.RedPic.TabStop = false;
+            // 
+            // GreenPic
+            // 
+            this.GreenPic.Image = ((System.Drawing.Image)(resources.GetObject("GreenPic.Image")));
+            this.GreenPic.Location = new System.Drawing.Point(382, 36);
+            this.GreenPic.Name = "GreenPic";
+            this.GreenPic.Size = new System.Drawing.Size(166, 328);
+            this.GreenPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.GreenPic.TabIndex = 5;
+            this.GreenPic.TabStop = false;
+            this.GreenPic.Visible = false;
+            // 
+            // YellowPic
+            // 
+            this.YellowPic.Image = ((System.Drawing.Image)(resources.GetObject("YellowPic.Image")));
+            this.YellowPic.Location = new System.Drawing.Point(382, 36);
+            this.YellowPic.Name = "YellowPic";
+            this.YellowPic.Size = new System.Drawing.Size(166, 328);
+            this.YellowPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.YellowPic.TabIndex = 1;
+            this.YellowPic.TabStop = false;
+            this.YellowPic.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 158);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Süre";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(92, 410);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(287, 63);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Başlat";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(428, 410);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(287, 63);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Bitir";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1156, 630);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RedPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YellowPic)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox YellowPic;
+        private System.Windows.Forms.PictureBox RedPic;
+        private System.Windows.Forms.PictureBox GreenPic;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- TrafikLambası/Form1.cs
+++ TrafikLambası/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media; //ses kütüphanesini ekledik
 
 namespace TrafikLambası
 {
@@ -14,7 +15,94 @@ namespace TrafikLambası
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();           
         }
+
+        int sayac=0;
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Start();           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            label1.Text = sayac.ToString();
+
+            //şarkıları burada yükledim
+            SoundPlayer kirmizi = new SoundPlayer();
+            SoundPlayer sari = new SoundPlayer();
+            SoundPlayer yesil = new SoundPlayer();
+            string yol1 = Application.StartupPath + "\\kirmizi.wav";
+            string yol2 = Application.StartupPath + "\\sari.wav";
+            string yol3 = Application.StartupPath + "\\yesil.wav";
+            kirmizi.SoundLocation = yol1;
+            sari.SoundLocation = yol2;
+            yesil.SoundLocation = yol3;       
+           
+
+            if ( sayac == 1 || sayac == 30 )
+            {
+                RedPic.Visible = true;
+                kirmizi.Play();
+                button4.Visible = false;
+            }
+
+            if (sayac == 61 || sayac == 90)
+            {
+                kirmizi.Stop();
+                RedPic.Visible = false;
+                sari.Play();
+                YellowPic.Visible = true;
+                button5.Visible = true;
+
+            }
+            if (sayac == 91 || sayac == 130)
+            {               
+                sari.Stop();
+                YellowPic.Visible = false;
+                yesil.Play();
+                GreenPic.Visible = true;
+                
+            }
+            if (sayac == 131 || sayac == 161)
+            {
+                yesil.Stop();
+                GreenPic.Visible = false;
+                sari.Play();
+                YellowPic.Visible = true;
+            }
+
+            if (sayac > 161)
+            {
+                sayac = 0;
+                sari.Stop();
+                YellowPic.Visible = false;
+                RedPic.Visible = true;
+                timer1.Stop();
+                button4.Visible = true;
+                button5.Visible = false;
+            }
+        }
+
+        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sayac = 0;           
+            YellowPic.Visible = false;
+            GreenPic.Visible = false;
+            RedPic.Visible = true;
+            timer1.Stop();
+            button4.Visible = true;
+            button5.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
